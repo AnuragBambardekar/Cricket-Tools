57efc45693e15776d41659d960fa08958e91162b
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-06-27 16:17:42 -0400

    all scenarios accounted for, for NRR calculation

--- nrr_calc_robust_ver.py
+++ nrr_calc_robust_ver.py
@@ -8,6 +8,43 @@ Adding Features:
 4. If a match is abandoned but a result decided by retrospectively applying Duckworth-Lewis, the number of overs assigned to each team for this calculation is the number of overs actually faced by Team 2. Team 1 is credited with Team 2's Par Score (the number of runs they would need to have reached from this number of overs and wickets lost if they were going to match Team 1's score), and the actual runs scored are used by Team 2 for Team 2's innings.
 """
 
+"""
+Scenarios:
+1. Side that bats first wins
+
+2. Side that bats second wins
+
+3. Side that bats first is bowled out, side batting second wins
+Team A bat first and are bowled out for 127 off 25.4 overs. Despite their run rate for the balls they faced being 127 / 25.667 = 4.95, because they were bowled out the entire 50 overs are added to their total overs faced tally for the tournament, and Team B are credited with having bowled 50 overs.
+Team B reach the target off 30.5 overs, ending with 128–4. Team B actually scored at a slower pace (128/30.833 = 4.15), however they managed to protect their wickets and win. Thus, only the 30.833 overs are added to the seasonal tally.
+
+4. Side that bats second is bowled out, side batting first therefore wins
+Team A bat first and set a formidable 295–5 off their complement of 50 overs. Therefore, for the tournament NRR calculations, 295 runs and 50 overs are added to Team A's runs scored/overs faced tally and Team B's runs conceded/overs bowled tally.
+Team B never get close, being bowled out for 116 off 35.4 overs. Therefore, as they were bowled out, 116 runs and 50 overs are added to Team A's runs conceded/overs bowled tally and Team B's runs scored/overs faced tally.
+
+5. Both sides are bowled out, side batting first therefore wins
+
+6. Game ends in a tie. [NRR=0]
+
+7. Interrupted game with revised D/L target
+In matches where Duckworth-Lewis revised targets are set due to interruptions which reduce the number of overs bowled, those revised targets and revised overs are used to calculate the NRR for both teams.
+For example, Team A are dismissed for 165 in 33.5 overs. Team B progresses to 120–0, but play is halted after 18 overs due to rain.
+Six overs are lost, and the target is reset to 150 from 44 overs, which Team B reach comfortably after 26.2 overs.
+
+Because the target was revised to 150 runs from 44 overs, Team A's total is reset to 149 from 44 overs, thus their RR =
+149/44 ≈ 3.39
+
+Team B's RR, however, is computed as normal: 
+150/26.33 ≈ 5.70
+
+8. Abandoned game recorded as No Result [scores in such games are immaterial to NRR calculations.]
+
+9. Abandoned game with retrospective D/L result
+Team A score 254 runs from their 50 overs. Team B have scored 172–4 from 30 overs when the match is abandoned.
+According to Duckworth-Lewis, 6 wickets and 20 overs in hand equates to 44.6% of resources, so Team B has used 55.4% of its resources, so their Par Score is 254 x 55.4% = 140.716 runs. As they are ahead of this, they are declared the winner.
+
+~ [Wikipedia]
+"""
 
 def calculate_net_run_rate(total_runs_scored, total_balls_faced, total_runs_conceded, total_balls_bowled):
     # print(total_runs_scored, total_balls_faced, total_runs_conceded, total_balls_bowled)
